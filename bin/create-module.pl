d0f251bd2655d3b632574f439811df99af160bbe
Pavel Shaydo <zwon@cpan.org>
2012-03-08 23:41:53 +0800

    github support

--- bin/create-module.pl
+++ bin/create-module.pl
@@ -6,6 +6,7 @@ use warnings;
 use ZWON::Module::Starter;
 use Getopt::Long;
 use Log::Log4perl qw(:easy get_logger);
+use Pod::Text;
 
 my $usage = <<EOT;
 create-module.pl [OPTIONS]
@@ -25,25 +26,39 @@ GetOptions(
     "author=s"       => \$author,
     "email=s"        => \$email,
     "distribution=s" => \my $distr,
-    "github=s" => \my $github,
-    help            => \my $help,
-    "quiet"         => \my $quiet,
+    "github=s"       => \my $github,
+    help             => \my $help,
+    "quiet"          => \my $quiet,
 ) or die $usage;
 
 die $usage if $help;
 die $usage unless $author && $email && $distr;
 Log::Log4perl->easy_init($DEBUG) unless $quiet;
 
-get_logger->info( "Creating $distr using author $author <$email>");
+get_logger->info("Creating $distr using author $author <$email>");
 
-my $starter = ZWON::Module::Starter->new( author => $author, email => $email, dist_name => $distr, github => $github );
+my $starter = ZWON::Module::Starter->new(
+    author    => $author,
+    email     => $email,
+    dist_name => $distr,
+    github    => $github
+);
 $starter->process_templates;
 
-chdir $started->dist_dir_name or die $!;
+chdir $starter->dist_dir_name or die $!;
+my $parser = Pod::Text->new;
+$parser->parse_from_file( 'README.pod', 'README' );
 system(qw(git init));
 system(qw(git add .));
-system($^X, "Makefile.PL");
+system( $^X, "Makefile.PL" );
 system(qw(make manifest));
 system(qw(git add MANIFEST));
-system('git', 'ci', '-m', "module skeleton");
+system( 'git', 'ci', '-m', "module skeleton" );
+
+if ($github) {
+    system( qw(git remote add origin), "git\@github.com:$github/perl-" . $starter->dist_dir_name );
+    get_logger->warn( "Added Github repository perl-"
+          . $starter->dist_dir_name . " as origin.");
+    get_logger->warn("You should manually create this repository before you can push" );
+}
 
