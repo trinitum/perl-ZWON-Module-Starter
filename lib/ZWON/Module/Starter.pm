340be3eb84921b676dfdac4543b980833ebc87a7
Pavel Shaydo <zwon@trinitum.org>
2015-01-06 23:43:13 +0100

    bump version v0.03

--- lib/ZWON/Module/Starter.pm
+++ lib/ZWON/Module/Starter.pm
@@ -13,11 +13,11 @@ ZWON::Module::Starter - Create distribution skeleton as ZWON likes
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
